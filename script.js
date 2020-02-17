89c6a043e49fda9036a34275af8da889f48df97c
matheus <dacachmatheus@gmail.com>
2020-02-17 18:14:53 -0300

    add snake and moving capabilities

--- script.js
+++ script.js
@@ -4,22 +4,30 @@ class Snake {
     this.size = size;
     this.x = x;
     this.y = y;
+    this.xs = 1; 
+    this.ys = 1; 
   }
 
-  moveLeft() {
-    this.x--;
-  }
-  moveRight() {
-    this.x++;
-  }
-  moveUp() {
-    this.y--;
-  }
-  moveDown() {
-    this.y++;
+  move() {
+
+    this.x += this.xs; 
+    this.y += this.ys; 
+    if (this.x >= 20 || this.x < 0)
+      this.x -= this.xs; 
+    if (this.y >= 20 || this.y < 0)
+      this.y -= this.ys; 
+    
+    
+  } 
+
+  show() {
+    gameGrid.grid[this.x][this.y] = 's'; 
   }
 
-  show() {}
+  update() {
+    snake.move(); 
+    snake.show(); 
+  }
 }
 
 class Grid {
@@ -33,7 +41,7 @@ class Grid {
       }
       this.grid.push(row);
     }
-    DOMStuff.constructGrid(this.size);
+    this.divGrid = DOMStuff.constructGrid(this.size);
   }
 
   addSnake(x, y) {
@@ -47,30 +55,86 @@ const DOMStuff = (function() {
     container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
     container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 
+    let divGrid = []; 
     for (let i = 0; i < size; i++) {
+      let row = []; 
       for (let j = 0; j < size; j++) {
         const div = document.createElement("div");
         div.classList.add("cell");
         container.appendChild(div);
+        row.push(div); 
       }
+      divGrid.push(row)
     }
+    return divGrid; 
   }
 
-  function drawSnake(x, y) {
-    for (let i = 0; i < gameGrid.size; i++) {
-      for (let j = 0; j < gameGrid.size; j++) {
-        if (gameGrid.grid[i][j] === "s") console.log("snake");
-      }
-    }
+  function addSnakeClass (div) {
+    div.classList.add('snake');
   }
+
+  function removeClasses (div) {
+    div.classList.remove('snake');
+  }
+
   return {
     container,
     constructGrid,
-    drawSnake
+    addSnakeClass,
+    removeClasses
   };
 })();
 
-gameGrid = new Grid(10);
+
+const gameFlow = (function () {
+  function clear () {
+    for (let i = 0; i < gameGrid.size; i++){
+      for (let j = 0; j < gameGrid.size; j++){
+        
+        if (gameGrid.grid[i][j] === 's') {
+          DOMStuff.removeClasses(gameGrid.divGrid[i][j])
+        } 
+        gameGrid.grid[i][j] = '';
+      }
+    }
+  }
+  
+  function draw () {
+    for (let i = 0; i < gameGrid.size; i++){
+      for (let j = 0; j < gameGrid.size; j++){
+        // console.log(gameGrid.grid[i][j])
+        // console.log(gameGrid.divGrid[i][j])
+        if (gameGrid.grid[i][j] === 's') {
+          DOMStuff.addSnakeClass(gameGrid.divGrid[i][j])
+        } 
+      }
+    }
+  }
+
+  function update () {
+    clear();
+    draw(); 
+  }
+  return {
+    clear,
+    draw,
+    update, 
+  }
+})(); 
+
+
+
+
+gameGrid = new Grid(20);
 snake = new Snake();
+
+function flow () {
+  gameFlow.clear(); 
+  gameGrid.addSnake(snake.x, snake.y); 
+  gameFlow.draw(); 
+  snake.update();
+}
 gameGrid.addSnake(snake.x, snake.y);
-DOMStuff.drawSnake();
+
+setInterval(flow, 1000)
+
